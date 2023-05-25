dd2e22e944722f836d28b2a1b08f1d67f49e181c
Airowl <airowl.gasga@gmail.com>
2023-05-25 08:16:43 +0200

    wip migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstName');
+            $table->string('middleName');
+            $table->string('lastName');
             $table->string('email')->unique();
             $table->text('avatar');
+            $table->date('registeredAt');
+            $table->date('lastLogin');
+            $table->text('intro');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ database/migrations/2023_05_22_185334_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('parentId');
+            $table->string('title');
+            $table->string('metaTitle');
+            $table->string('slug');
+            $table->tinyText('summury');
+            $table->tinyInteger('published');
+            $table->dateTime('publishedAt');
+            $table->text('content');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
